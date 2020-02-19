9e69987ea3df34c87a99217b9610d342aa8e0ad6
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-19 09:35:03 -0700

    Test for recomment exhibits Logic time!

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -30,6 +30,19 @@ class MusemTest < Minitest::Test
 
     assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
   end
+
+  def test_can_recommend_exhibits_based_on_interests
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
+
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@patron_1)
+    assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
+  end
 end
 
 # pry(main)> patron_1 = Patron.new("Bob", 20)
