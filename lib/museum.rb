73276c86086ead3f65af327489915e0e2e4684a6
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-19 09:44:06 -0700

    Working on test for lottery Mocks.... atleast iteration 4

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,12 @@
 class Museum
     attr_reader :name,
-                :exhibits
+                :exhibits,
+                :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -13,5 +15,9 @@ class Museum
 
   def recommend_exhibits(patron)
     @exhibits.find_all { |exhibit| patron.interests.include?(exhibit.name) }
+  end
+
+  def admit(patron)
+    @patrons << patron
   end
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -12,6 +12,7 @@ class MusemTest < Minitest::Test
      @imax = Exhibit.new({name: "IMAX",cost: 15})
      @patron_1 = Patron.new("Bob", 20)
      @patron_2 = Patron.new("Sally", 20)
+     @patron_3 = Patron.new("Johnny", 5)
   end
 
   def test_it_exists
@@ -21,6 +22,7 @@ class MusemTest < Minitest::Test
   def test_attributes
     assert_equal "Denver Museum of Nature and Science", @dmns.name
     assert_equal [], @dmns.exhibits
+    assert_equal [], @dmns.patrons
   end
 
   def test_exhibits_array_initializes_empty_and_can_be_added_to
@@ -43,22 +45,63 @@ class MusemTest < Minitest::Test
     assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@patron_1)
     assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
   end
-end
 
-# pry(main)> patron_1 = Patron.new("Bob", 20)
-# # => #<Patron:0x00007fb400a51cc8...>
+  def test_patrons_can_be_admitted
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+
+    assert_equal [@patron_1, @patron_2, @patron_3], @dmns.patrons
+  end
 #
-# pry(main)> patron_1.add_interest("Dead Sea Scrolls")
+#   def test_can_draw_lottery_winner
+#     @dmns.add_exhibit(@gems_and_minerals)
+#     @dmns.add_exhibit(@dead_sea_scrolls)
+#     @dmns.add_exhibit(@imax)
+#
+#     @patron_1.add_interest("Dead Sea Scrolls")
+#     @patron_1.add_interest("Gems and Minerals")
+#     @patron_2.add_interest("IMAX")
+#
+#
+#
+#
+#
+#     @dmns.draw_lottery_winner(@dead_sea_scrolls)
+#   end
+#
+#
+end 
+
+
+# pry(main)> dmns.add_exhibit(gems_and_minerals)
+#
+# pry(main)> dmns.add_exhibit(dead_sea_scrolls)
+#
+# pry(main)> dmns.add_exhibit(imax)
+#
+# pry(main)> dmns.patrons
+# # => []
+#
+# pry(main)> patron_1 = Patron.new("Bob", 0)
+# # => #<Patron:0x00007fb2011455b8...>
 #
 # pry(main)> patron_1.add_interest("Gems and Minerals")
 #
+# pry(main)> patron_1.add_interest("Dead Sea Scrolls")
+#
 # pry(main)> patron_2 = Patron.new("Sally", 20)
-# # => #<Patron:0x00007fb400036338...>
+# # => #<Patron:0x00007fb20227f8b0...>
 #
-# pry(main)> patron_2.add_interest("IMAX")
+# pry(main)> patron_2.add_interest("Dead Sea Scrolls")
 #
-# pry(main)> dmns.recommend_exhibits(patron_1)
-# # => [#<Exhibit:0x00007fb400bbcdd8...>, #<Exhibit:0x00007fb400b851f8...>]
+# pry(main)> patron_3 = Patron.new("Johnny", 5)
+# # => #<Patron:0x6666fb20114megan...>
 #
-# pry(main)> dmns.recommend_exhibits(patron_2)
-# # => [#<Exhibit:0x00007fb400acc590...>]
+# pry(main)> patron_3.add_interest("Dead Sea Scrolls")
+#
+# pry(main)> dmns.admit(patron_1)
+#
+# pry(main)> dmns.admit(patron_2)
+#
+# pry(main)> dmns.admit(patron_3)
