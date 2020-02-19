31262ffe4dccdb966d3e384cc2c4b8b1391aa615
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-19 10:37:07 -0700

    Test created and passing for ticket lottery contestants

--- lib/museum.rb
+++ lib/museum.rb
@@ -27,4 +27,8 @@ class Museum
       exh_interests
     end
   end
+
+  def ticket_lottery_contestants(interest)
+    @patrons.find_all { |patron| patron.interests.include?(interest.name) }
+  end
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -6,13 +6,13 @@ require './lib/museum'
 class MusemTest < Minitest::Test
 
   def setup
-     @dmns = Museum.new("Denver Museum of Nature and Science")
-     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-     @imax = Exhibit.new({name: "IMAX",cost: 15})
-     @patron_1 = Patron.new("Bob", 20)
-     @patron_2 = Patron.new("Sally", 20)
-     @patron_3 = Patron.new("Johnny", 5)
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_3 = Patron.new("Johnny", 5)
   end
 
   def test_it_exists
@@ -70,36 +70,46 @@ class MusemTest < Minitest::Test
       @gems_and_minerals => [@patron_1],
       @dead_sea_scrolls => [@patron_1, @patron_2, @patron_3],
       @imax => []
-      }
+    }
 
     assert_equal expected, @dmns.patrons_by_exhibit_interest
   end
 
+  def test_can_return_lottery_ticket_contestants_based_on_interests
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_3.add_interest("Dead Sea Scrolls")
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_3)
 
+    assert_equal [@patron_1, @patron_3], @dmns.ticket_lottery_contestants(@dead_sea_scrolls)
+  end
 
 end
-  #
-  # def test_can_draw_lottery_winner
-  #   # @dmns.add_exhibit(@gems_and_minerals)
-  #   # @dmns.add_exhibit(@dead_sea_scrolls)
-  #   # @dmns.add_exhibit(@imax)
-  #   #
-  #   # @patron_1.add_interest("Dead Sea Scrolls")
-  #   # @patron_1.add_interest("Gems and Minerals")
-  #   # @patron_2.add_interest("IMAX")
-  #   # @patron_3.add_interest("Dead Sea Scrolls")
-  #
-  #   @dmns.stubs(:ticket_lottery_contestants).returns(@patron1)
-  #
-  #   assert_equal @patron1, @dmns.draw_lottery_winner(@dead_sea_scrolls)
-  # end
-  #
-  # def test_can_announce_lottery_winner
-  #   @dmns.stubs(:draw_lottery_winner).returns(@patron_1)
-  #   expected = "#{@patron_1} has won the Dead Sea Scrolls exhibit lottery"
-  #
-  #   assert_equal expected, @dmns.announce_lottery_winner(@dead_sea_scrolls)
-  # end
+# def test_can_draw_lottery_winner
+#   # @dmns.add_exhibit(@gems_and_minerals)
+#   # @dmns.add_exhibit(@dead_sea_scrolls)
+#   # @dmns.add_exhibit(@imax)
+#   #
+#   # @patron_1.add_interest("Dead Sea Scrolls")
+#   # @patron_1.add_interest("Gems and Minerals")
+#   # @patron_2.add_interest("IMAX")
+#   # @patron_3.add_interest("Dead Sea Scrolls")
+#
+#   @dmns.stubs(:ticket_lottery_contestants).returns(@patron1)
+#
+#   assert_equal @patron1, @dmns.draw_lottery_winner(@dead_sea_scrolls)
+# end
+
+# def test_can_announce_lottery_winner
+#   @dmns.stubs(:draw_lottery_winner).returns(@patron_1)
+#   expected = "#{@patron_1} has won the Dead Sea Scrolls exhibit lottery"
+#
+#   assert_equal expected, @dmns.announce_lottery_winner(@dead_sea_scrolls)
+# end
 
 
 # pry(main)> dmns.add_exhibit(gems_and_minerals)
